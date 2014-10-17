13adb45dd8be69ed5d828d11db777a855067bba5
tunderdomb <tunderdomb@gmail.com>
2014-10-17 11:35:12 +0200

    set caching to manifest default

--- engines/dustin.js
+++ engines/dustin.js
@@ -1,11 +1,9 @@
-var DEBUG = process.env.NODE_ENV == "development"
-
 module.exports = function( app, manifest ){
   //  debugger
   var adapter = require("dustin")({
-    cache: !DEBUG,
-    whiteSpace: false,
-    helpers: "helpers/node/*.js",
+    cache: !manifest.env.development,
+    whiteSpace: true,
+//    helpers: "helpers/node/*.js",
     views: manifest.engine.views
   })
   return function( path, context, cb ){
